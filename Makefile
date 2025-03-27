62a4f868c181cc1f9f8ca52190f2ba643629f7ee
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2025-03-27 16:10:15 +0600

    learning...

--- Makefile
+++ Makefile
@@ -2,4 +2,8 @@ dev:
 	gcc -Wall *.c -o learnc
 	./learnc
 
-.PHONY: dev
+text_search:
+	gcc -Wall ./projects/cli_text_search/*.c -o text_search
+	./text_search
+
+.PHONY: dev, text_search
--- main.c
+++ main.c
@@ -1,11 +1,39 @@
-#include "modules.h"
+// #include "modules.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <unistd.h>
+
+struct MyArr {
+	int v[];
+	int len;
+};
+
+void calc_arr_len(int *arr) {
+	int i;
+	printf("accessing the actual value\n");
+	for (i = 0; i < sizeof(*arr); i++) {
+		printf("%i\n", arr[i]);
+	};
+
+	printf("accessing the pointer only\n");
+	for (i = 0; i < sizeof(arr); i++) {
+		printf("%i\n", arr[i]);
+	};
+
+	int *arr_ptr = arr;
+	int the_arr = *arr;
+}
 
 int main() {
 	printf("Learning C\n");
-	read_file("input.txt");
+
+	int mylist[] = {1, 2, 3, 4, 5, 6};
+	printf("Number of items in the list: %lu\n",
+		   sizeof(mylist) / sizeof(mylist[0]));
+
+	// printf("%lu\n", sizeof(int *));
+	// printf("%lu\n", sizeof(int));
+	calc_arr_len(mylist);
 	return 0;
 };
