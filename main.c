f150a42e642cfccf749a09e4ba74532e0a836430
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2025-03-29 05:47:53 +0600

    learning scanf

--- main.c
+++ main.c
@@ -21,5 +21,43 @@ int main() {
 
 	open_read_print("input.txt");
 
+	printf("--------------");
+
+	void get_min_max_int_from_inputs();
+	get_min_max_int_from_inputs();
+
+	void simple_guess_game();
+	simple_guess_game();
+
 	return 0;
+};
+
+void get_min_max_int_from_inputs() {
+	int first = 1, min, max, val;
+	printf("Enter numbers then use ctrl+D to stop\n");
+	printf(": ");
+	while (scanf("%d", &val) != EOF) {
+		if (first || val < min) {
+			min = val;
+		} else if (first || val > max) {
+			max = val;
+		}
+		first = 0;
+	}
+	printf("min: %i, max: %i\n", min, max);
+};
+
+void simple_guess_game() {
+	int guess, val = 54;
+	printf("Enter your guess\n");
+	while (scanf("%d", &guess) != EOF) {
+		if (guess > val) {
+			printf("Your guess is too high!\n");
+		} else if (guess < val) {
+			printf("Your guess is too low!\n");
+		} else {
+			printf("You've guess successfully! \n");
+			break;
+		}
+	}
 };
--- working_with_files.c
+++ working_with_files.c
@@ -2,4 +2,17 @@
 
 void open_read_print(char *filename) {
 	printf("Opening file '%s' in read mode\n", filename);
+
+	int readlen = 10;
+	char buf[readlen];
+	FILE *f;
+	f = fopen(filename, "r");
+	while (fgets(buf, readlen, f) != NULL) {
+		printf("%s", buf);
+	}
 };
+
+/* void write_file(char *filename, char *content) {
+	int readlen = 10;
+	char buf[readlen];
+}; */
